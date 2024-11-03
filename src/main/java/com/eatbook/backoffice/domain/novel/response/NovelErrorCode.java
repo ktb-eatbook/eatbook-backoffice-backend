6c70dd83a0544daa0a8e9665dc7f022010f043d6
KIM TAE HYEONG <thk12378905@gmail.com>
2024-11-04 07:15:06 +0900

    feat: Episode 생성 시 발생할 수 있는 예외 추가

--- src/main/java/com/eatbook/backoffice/domain/novel/response/NovelErrorCode.java
+++ src/main/java/com/eatbook/backoffice/domain/novel/response/NovelErrorCode.java
@@ -9,6 +9,7 @@ import lombok.Getter;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum NovelErrorCode implements StatusCode {
     NOVEL_ALREADY_EXISTS("NOVEL_ALREADY_EXISTS", "이미 존재하는 소설입니다. 이미 같은 제목과 작가를 가진 소설이 존재합니다."),
+    NOVEL_NOT_FOUND("NOVEL_NOT_FOUND", "존재하지 않는 소설입니다."),
     PRESIGNED_URL_GENERATION_FAILED("PRESIGNED_URL_GENERATION_FAILED", "presinged url 생성에 실패했습니다. 에러메세지: %s"),
     S3_PRE_SIGNED_URL_GENERATION_FAILED("S3_PRE_SIGNED_URL_GENERATION_FAILED", "S3 presinged url 생성에 실패했습니다."),
 
