d61fd5c622bb3eb3bb1f25603c6fa8a50022d498
KIM TAE HYEONG <thk12378905@gmail.com>
2024-11-08 03:05:25 +0900

    refactor: 카운트 쿼리 최적화

--- src/main/java/com/eatbook/backoffice/domain/novel/repository/NovelRepository.java
+++ src/main/java/com/eatbook/backoffice/domain/novel/repository/NovelRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface NovelRepository extends JpaRepository<Novel, String> {
-    @Query("SELECT n FROM Novel n JOIN FETCH n.novelAuthors JOIN FETCH n.novelCategories")
+    @Query(
+            value = "SELECT n FROM Novel n JOIN FETCH n.novelAuthors JOIN FETCH n.novelCategories",
+            countQuery = "SELECT count(n) FROM Novel n")
     Page<Novel> findAllWithAuthorsAndCategories(Pageable pageable);
 }
