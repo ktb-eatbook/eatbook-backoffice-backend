ef81efd98c4df465d9f1088cafe0497e2e3d0dec
KIM TAE HYEONG <thk12378905@gmail.com>
2024-11-11 16:04:26 +0900

    chore: 불필요한 띄어쓰기 2개 들어간 부분 수정

--- src/main/java/com/eatbook/backoffice/domain/novel/exception/PageOutOfBoundException.java
+++ src/main/java/com/eatbook/backoffice/domain/novel/exception/PageOutOfBoundException.java
@@ -3,7 +3,7 @@ package com.eatbook.backoffice.domain.novel.exception;
 import com.eatbook.backoffice.global.exception.exceptions.BusinessException;
 import com.eatbook.backoffice.global.response.StatusCode;
 
-public class PageOutOfBoundException  extends BusinessException {
+public class PageOutOfBoundException extends BusinessException {
     public PageOutOfBoundException(StatusCode code) {
         super(code);
     }
