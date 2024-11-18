b9ba66f88e9266c31cf96b24d7620f362f0d79d4
KIM TAE HYEONG <thk12378905@gmail.com>
2024-11-18 14:01:26 +0900

    feat: 소설 댓글 리스트 조회 성공 코드 추가

--- src/main/java/com/eatbook/backoffice/domain/novel/response/NovelSuccessCode.java
+++ src/main/java/com/eatbook/backoffice/domain/novel/response/NovelSuccessCode.java
@@ -11,6 +11,7 @@ public enum NovelSuccessCode implements StatusCode {
     NOVEL_CREATED("NOVEL_CREATED", "소설이 성공적으로 생성되었습니다."),
     GET_NOVEL_LIST("GET_NOVEL_LIST", "소설 목록을 성공적으로 조회했습니다."),
     GET_NOVEL_DETAIL("GET_NOVEL_DETAIL", "소설 상세 정보를 성공적으로 조회했습니다."),
+    GET_NOVEL_COMMENT_LIST("GET_NOVEL_COMMENT_LIST", "소설 댓글 목록을 성공적으로 조회했습니다."),
     ;
 
     private final String code;
