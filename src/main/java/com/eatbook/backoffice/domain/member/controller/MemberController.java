8a4fcdab0973daf32b118e11e5b3045f51afcaf7
KIM TAE HYEONG <thk12378905@gmail.com>
2024-12-06 09:50:04 +0900

    feat:권한 부여 기능 관련 controller, service, repository 로직 추가

--- src/main/java/com/eatbook/backoffice/domain/member/controller/MemberController.java
+++ src/main/java/com/eatbook/backoffice/domain/member/controller/MemberController.java
@@ -11,12 +11,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static com.eatbook.backoffice.domain.member.response.MemberSuccessCode.GET_MEMBER_LIST;
+import static com.eatbook.backoffice.domain.member.response.MemberSuccessCode.UPDATE_MEMBER_ROLE;
 
 @RestController
 @RequiredArgsConstructor
@@ -44,5 +42,15 @@ public class MemberController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(ApiResponse.of(GET_MEMBER_LIST, memberList));
+    }
+
+    @PutMapping("/members/{memberId}/roles/{role}")
+    public ResponseEntity<ApiResponse> updateMemberRole(
+                                                        @PathVariable("memberId") String memberId,
+                                                        @PathVariable("role") String role) {
+        memberService.updateMemberRole(memberId, role);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ApiResponse.of(UPDATE_MEMBER_ROLE));
     }
 }
--- src/main/java/com/eatbook/backoffice/domain/member/repository/MemberRepository.java
+++ src/main/java/com/eatbook/backoffice/domain/member/repository/MemberRepository.java
@@ -2,10 +2,20 @@ package com.eatbook.backoffice.domain.member.repository;
 
 import com.eatbook.backoffice.domain.member.repository.queryDSL.MemberCustomRepository;
 import com.eatbook.backoffice.entity.Member;
+import com.eatbook.backoffice.entity.constant.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, String>, MemberCustomRepository {
-    Optional<Member> findMembersByEmail(String email);
+    Optional<Member> findMemberByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    @Modifying
+    @Query("UPDATE Member m SET m.role = :role WHERE m.id = :id")
+    void updateMemberRole(@Param("id") String id, @Param("role") Role role);
 }
