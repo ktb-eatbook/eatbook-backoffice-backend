76a8828bbad08013ce34b67a9025990390ebeb56
KIM TAE HYEONG <thk12378905@gmail.com>
2024-12-09 15:06:17 +0900

    refactor: EpisodeDetailResponse의 팩토리 메서드 패턴에서 빌더 패턴으로 변경

--- src/main/java/com/eatbook/backoffice/domain/episode/dto/EpisodeDetailResponse.java
+++ src/main/java/com/eatbook/backoffice/domain/episode/dto/EpisodeDetailResponse.java
@@ -1,9 +1,11 @@
 package com.eatbook.backoffice.domain.episode.dto;
 
 import com.eatbook.backoffice.entity.constant.ReleaseStatus;
+import lombok.Builder;
 
 import java.time.LocalDateTime;
 
+@Builder
 public record EpisodeDetailResponse(
         String id,
         String title,
@@ -16,29 +18,4 @@ public record EpisodeDetailResponse(
         LocalDateTime createdAt,
         LocalDateTime updatedAt
 ) {
-    public static EpisodeDetailResponse of(
-            String id,
-            String title,
-            int chapter,
-            LocalDateTime releaseDate,
-            LocalDateTime scheduledDate,
-            ReleaseStatus releaseStatus,
-            String novelId,
-            int viewCount,
-            LocalDateTime createdAt,
-            LocalDateTime updatedAt
-    ) {
-        return new EpisodeDetailResponse(
-                id,
-                title,
-                chapter,
-                releaseDate,
-                scheduledDate,
-                releaseStatus,
-                novelId,
-                viewCount,
-                createdAt,
-                updatedAt
-        );
-    }
 }
